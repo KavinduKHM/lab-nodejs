d98d7976a273b0a5cae097d6c0197bf581c18766
Kavindu Kalhara <kavinduhewamadduma@gmail.com>
2026-02-19 20:45:45 +0530

    Implemented HTTP request example

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-console.log("Hello Node.js Lab!");
+const https = require('https');
 
-const fs = require('fs');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
 
-fs.readFile('ReadFile.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
 });
-
-
-
-const fs1 = require('fs');
-
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
-});
-
-
-const http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);
-
-console.log("Server running at http://localhost:8080");
