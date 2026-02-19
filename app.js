068cf0e7a3efb0690f342d51d48c516714fa487f
Kavindu Kalhara <kavinduhewamadduma@gmail.com>
2026-02-19 20:42:04 +0530

    Implemented write file example

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello Node.js Lab!");
+
+const fs = require('fs');
+
+fs.readFile('ReadFile.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+
+
+const fs = require('fs');
+
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
+});
